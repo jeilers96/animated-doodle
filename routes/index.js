6495efaf7d054ac455bea1e755c59b3fc567f7a6
jeilers96 <jeilers96@gmail.com>
2018-07-26 21:27:14 -0500

    add dynamic genres that link to filtered collection

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Josh's Film Collection" });
-});
-
-router.get('/filmlist', function(req, res) {
+router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('films');
-    collection.find({},{},function(e,docs){
-        res.render('filmlist', {
-            "filmlist" : docs
+    var genreCollection = db.get('genres');
+    collection.find({},{},function(e,films){
+      genreCollection.find({},{},function(e, genres) {
+        res.render('index', {
+            "filmlist" : films,
+            "genres" : genres,
+            title: "Josh's Film Collection"
         });
+      });
+    });
+});
+
+router.get('/genre/:genre', function(req, res) {
+    var db = req.db;
+    var collection = db.get('films');
+    var genreCollection = db.get('genres');
+    collection.find({Genre: req.params.genre},{},function(e,films){
+      genreCollection.find({},{},function(e, genres) {
+        res.render('index', {
+            "filmlist" : films,
+            "genres" : genres,
+            title: "Josh's Film Collection"
+        });
+      });
     });
 });
 
