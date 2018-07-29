fa5f37d8d2276d7ba3376bda83b69661cfd63519
jeilers96 <jeilers96@gmail.com>
2018-07-29 13:49:25 -0500

    work on filtering and sorting data

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,22 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('films');
-    var genreCollection = db.get('genres');
-    collection.find({},{},function(e,films){
-      genreCollection.find({},{},function(e, genres) {
+    var options = {};
+    var query = {};
+    if(req.query.genre != null) {
+      query = { Genre: req.query.genre };
+    }
+    if(req.query.sortby == "alphabetical") {
+      options = { sort: { Title: 1 } };
+    } else if(req.query.sortby == "releaseYear") {
+      options = { sort: {Year: 1} };
+    } else if(req.query.sortby == "Runtime") {
+      options = { sort: {Runtime: 1} };
+    }
+    collection.find(query, options, function(e, films) {
+      collection.distinct('Genre', function(e, genres) {
         res.render('index', {
-            "filmlist" : films,
-            "genres" : genres,
-            title: "Josh's Film Collection"
-        });
-      });
-    });
-});
-
-router.get('/genre/:genre', function(req, res) {
-    var db = req.db;
-    var collection = db.get('films');
-    var genreCollection = db.get('genres');
-    collection.find({Genre: req.params.genre},{},function(e,films){
-      genreCollection.find({},{},function(e, genres) {
-        res.render('index', {
+            "currentGenre" : "All",
             "filmlist" : films,
             "genres" : genres,
             title: "Josh's Film Collection"
