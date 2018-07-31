02e08f1e84e32b769e4c32a9c155a35b2893a755
jeilers96 <jeilers96@gmail.com>
2018-07-30 19:26:31 -0500

    add reverse sorting for all different sort methods

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ router.get('/', function(req, res) {
     var collection = db.get('films');
     var options = {};
     var query = {};
+    var currentGenre = "All"
+    var currentQueryParams = "";
+    var url = require('url').parse(req.url).query;
+    console.log(url);
     if(req.query.genre != null) {
       query = { Genre: req.query.genre };
+      currentGenre = req.query.genre;
     }
     if(req.query.sortby == "alphabetical") {
       options = { sort: { Title: 1 } };
@@ -15,11 +20,18 @@ router.get('/', function(req, res) {
       options = { sort: {Year: 1} };
     } else if(req.query.sortby == "Runtime") {
       options = { sort: {Runtime: 1} };
+    } else if(req.query.sortby == "alphabeticalRev") {
+      options = { sort: { Title: -1 } };
+    } else if(req.query.sortby == "releaseYearRev") {
+      options = { sort: {Year: -1} };
+    } else if(req.query.sortby == "RuntimeRev") {
+      options = { sort: {Runtime: -1} };
     }
     collection.find(query, options, function(e, films) {
       collection.distinct('Genre', function(e, genres) {
         res.render('index', {
-            "currentGenre" : "All",
+            "currentQueryParams" : currentQueryParams,
+            "currentGenre" : currentGenre,
             "filmlist" : films,
             "genres" : genres,
             title: "Josh's Film Collection"
