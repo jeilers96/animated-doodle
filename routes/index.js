52b8ca9f031ed0f04d110f7f80217b2105f968f7
jeilers96 <jeilers96@gmail.com>
2018-08-14 20:02:47 -0500

    add director filter

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+
+    //setup db connection
     var db = req.db;
     var collection = db.get('films');
+
+    //setup default variables
     var options = {};
     var query = {};
-    var currentGenre = "All"
+    var currentGenre = "All";
+    var currentDirector = "All";
     var currentQueryParams = "";
-    var url = require('url').parse(req.url).query;
-    console.log(url);
+
+    //set query and current filter
     if(req.query.genre != null) {
       query = { Genre: req.query.genre };
       currentGenre = req.query.genre;
     }
+    if(req.query.director != null) {
+      query = { Director: req.query.director};
+      currentDirector = req.query.director;
+    }
+
+    //set query to sort correctly
     if(req.query.sortby == "alphabetical") {
       options = { sort: { Title: 1 } };
     } else if(req.query.sortby == "releaseYear") {
@@ -27,14 +38,20 @@ router.get('/', function(req, res) {
     } else if(req.query.sortby == "RuntimeRev") {
       options = { sort: {Runtime: -1} };
     }
+
+    //get collection and send correct attributes to View
     collection.find(query, options, function(e, films) {
       collection.distinct('Genre', function(e, genres) {
-        res.render('index', {
-            "currentQueryParams" : currentQueryParams,
-            "currentGenre" : currentGenre,
-            "filmlist" : films,
-            "genres" : genres,
-            title: "Josh's Film Collection"
+        collection.distinct('Director', function(e, directors) {
+          res.render('index', {
+              "currentQueryParams" : currentQueryParams,
+              "currentGenre" : currentGenre,
+              "currentDirector" : currentDirector,
+              "filmlist" : films,
+              "genres" : genres,
+              "directors" : directors,
+              title: "Josh's Film Collection"
+          });
         });
       });
     });
